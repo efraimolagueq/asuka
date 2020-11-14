a2effd5d309153b3df0d71c3393adad3e05ddec6
Efraim Olague <olagefra.7@gmail.com>
2020-11-13 20:47:00 -0600

    Corregido mensaje global

--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 import os, random
 
+
 client = commands.Bot(command_prefix=".")
 package_dir = os.path.dirname(os.path.abspath(__file__))
 token = os.getenv("DISCORD_BOT_TOKEN")
@@ -12,6 +13,7 @@ scheduler = AsyncIOScheduler({'apscheduler.timezone': 'America/Mexico_City'})
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.idle, activity=discord.Game("Amando las waifus .help"))
+    print("Asuka Ready")
 
 
 @client.command()
@@ -19,9 +21,50 @@ async def ping(ctx):
     await ctx.send(f"🏓 Pong con {str(round(client.latency, 2))} de latencia")
 
 
-@client.command(name="quiensoy")
-async def quiensoy(ctx):
-    await ctx.send(f"Tu eres {ctx.message.author.name}")
+# @client.command(name="quiensoy")
+# async def quiensoy(ctx):
+#     await ctx.send(f"Tu eres {ctx.message.author.name}")
+
+@client.command(name="coco")
+async def coco(ctx):
+    await ctx.send("https://www.youtube.com/watch?v=BxlCm7iR6MY&feature=share")
+
+
+def get_general_channels():
+    channels = client.get_all_channels()
+    message_channels = []
+    for channel in channels:
+        if channel.name == 'general':
+            message_channels.append(channel)
+    return message_channels
+
+
+async def feliz_jueves():
+    message_channels = get_general_channels()
+    for general in message_channels:
+        await general.send("Feliz Jueves Bakaa!!! <3 @everyone", file=discord.File(select_random_dia('jueves')))
+
+
+async def feliz_viernes():
+    message_channels = get_general_channels()
+    for general in message_channels:
+        await general.send("Gracias a Shrek es viernes!!! @everyone",
+                               file=discord.File(select_random_dia('viernes')))
+
+def select_random_dia(dia):
+    if dia == 'jueves':
+        imagenes = os.path.join(package_dir, 'images/asuka/')
+    else:
+        imagenes = os.path.join(package_dir, 'images/shrek/')
+    rdn_image = random.choice(os.listdir(imagenes))
+    return os.path.join(imagenes, rdn_image)
+
+
+scheduler.add_job(feliz_jueves, 'cron', day_of_week='thu', hour=0, minute=0)
+scheduler.add_job(feliz_viernes, 'cron', day_of_week='fri', hour=0, minute=0)
+
+scheduler.start()
+client.run(token)
 
 
 # @tasks.loop(hours=24)
@@ -42,41 +85,17 @@ async def quiensoy(ctx):
 #     await message_channel.send("feliz domingo")
 # scheduler.add_job(feliz_jueves, 'cron', day_of_week='thu', hour=0, minute=0)
 
-
-def get_general_channel():
-    channels = client.get_all_channels()
-    message_channel = None
-    for channel in channels:
-        if channel.name == 'general':
-            message_channel = channel
-    if message_channel is None:
-        message_channel = channels[0]
-    return message_channel
-
-
-def select_random_dia(dia):
-    if dia == 'jueves':
-        imagenes = os.path.join(package_dir, 'images/asuka/')
-    else:
-        imagenes = os.path.join(package_dir, 'images/shrek/')
-    rdn_image = random.choice(os.listdir(imagenes))
-    return os.path.join(imagenes, rdn_image)
+# @bot.command(pass_context=True)
+# async def broadcast(ctx, *, msg):
+#     for server in bot.servers:
+#         for channel in server.channels:
+#             try:
+#                 await bot.send_message(channel, msg)
+#             except Exception:
+#                 continue
+#             else:
+#                 break
 
 
-async def feliz_jueves():
-    message_channel = get_general_channel()
-    await message_channel.send("Feliz Jueves Bakaa!!! <3 @everyone", file=discord.File(select_random_dia('jueves')))
 
-
-async def feliz_viernes():
-    message_channel = get_general_channel()
-    await message_channel.send("Gracias a Shrek es viernes!!! @everyone",
-                               file=discord.File(select_random_dia('viernes')))
-
-
-scheduler.add_job(feliz_jueves, 'cron', day_of_week='thu', hour=0, minute=1)
-scheduler.add_job(feliz_viernes, 'cron', day_of_week='fri', hour=0, minute=1)
-
-scheduler.start()
-client.run(token)
 
